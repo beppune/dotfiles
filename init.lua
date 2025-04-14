28b7275ce76d11715720ff925101b2762000eb77
beppe <manzo.giuseppe@gmail.com>
2025-04-14 17:22:45 +0200

    init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.cmd([[
+
+set tabstop=4
+set shiftwidth=4
+set number
+set relativenumber
+
+inoremap jj <Esc>
+inoremap <C-o> <Esc>o}<Esc>O
+inoremap <C-s> <Esc>:w<Enter>i
+inoremap <C-q> <Esc>:wq<Enter>
+
+]])
