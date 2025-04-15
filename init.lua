f2c0ac8378b4989f1e5d1d6916ea2d536d04be90
beppe <manzo.giuseppe@gmail.com>
2025-04-15 14:17:44 +0200

    autocmd

--- init.lua
+++ init.lua
@@ -8,9 +8,14 @@ set relativenumber
 inoremap jj <Esc>
 inoremap <C-o> <Esc>o}<Esc>O
 inoremap <C-s> <Esc>:w<Enter>i
-inoremap <C-q> <Esc>:wq<Enter>
 
 nnoremap <C-q> <Esc>:q!<Enter>
 nnoremap <C-h> <Esc>:noh<Enter>
+
+augroup Folds
+	au!
+	autocmd FileType c,rust set foldmethod=indent
+	autocmd FileType c,rust set foldcolumn=1
+augroup END
 
 ]])
