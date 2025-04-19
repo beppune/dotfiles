9cb8d6cf96f7c0f7e94a7ce6388add4e9eb8ae72
beppune <manzo.giuseppe@gmail.com>
2025-04-19 09:32:27 +0200

    lua

--- init.lua
+++ init.lua
@@ -1,45 +1,54 @@
 
+-- Aliases
 o = vim.o
-a = vim.api
 c = vim.cmd
+a = vim.api
 g = vim.g
 
-o.tabstop = 4
-o.shiftwidth = 4
+-- Settings
+c.colorscheme('retrobox')
+c.language('en')
 o.number = true
 o.relativenumber = true
-o.mouse = ""
+o.tabstop = 4
+o.shiftwidth = 4
 
-c.colorscheme('blue')
-c.language('en')
+-- MAPPINGS
 
+local function map_key(mode, lhs, rhs, descj)
+	a.nvim_set_keymap(mode, lhs, rhs, { 
+		noremap = true,
+		desc = desc
+	})
+end
+
+-- Insert Mode
+map_key('i', 'jj', '<Esc>', "Use 'jj' in insert mode to switch in normal mode")
+map_key('i', '<C-s>', '<Esc>:w<Esc>i', "In insert mode save changes and get back in insert mode")
+
+-- Windows
 g.mapleader = " "
-a.nvim_set_keymap('n', '<Leader>v', '<C-w>v', {noremap = true})
-a.nvim_set_keymap('n', '<Leader>w', '<C-w>w', {noremap = true})
-a.nvim_set_keymap('n', '<Leader>r', '<C-w>r', {noremap = true})
+map_key('n', '<Leader>v', '<C-w>v', "Split Window Vertically")
+map_key('n', '<Leader>r', '<C-w>r', "Rotate Windows")
+map_key('n', '<Leader>w', '<C-w>w', "Switch to next window")
 
-a.nvim_set_keymap('i', 'jj', '<Esc>', {noremap = true})
-a.nvim_set_keymap('i', '<C-o>', '<Esc>o}<Esc>O', {noremap = true})
-a.nvim_set_keymap('i', '<C-s>', '<Esc>:w<Enter>i', {noremap = true})
+-- Scroll in Normal Mode
+-- g.mapleader = " "
+map_key('n', '<Leader>j', '<C-d>', "Scroll down half window")
+map_key('n', '<Leader>k', '<C-u>', "Scroll up hal window")
+map_key('n', '<Leader>h', '<C-e>', "Scroll down one line")
+map_key('n', '<Leader>l', '<C-y>', "Scroll up one line")
 
-a.nvim_set_keymap('n', '<C-h>', '<Esc>:noh<Enter>', {noremap = true})
+-- Tabs
+-- g.mapleader = " "
+map_key('n', '<Leader>t', ':tabnew<Enter>', "New Tab")
+map_key('n', '<Leader>c', ':tabclose<Enter>', "Close Tab")
+map_key('n', '<Leader>n', ':tabnext<Enter>', "Next Tab")
+
+-- Utils
+map_key('n', '<C-h>', ':nohlsearch<Enter>', "Clear search highlights")
 
 
--- AUTOCOMMAND GROUP FOR FOLDING
+vim.api.utils = require('utils')
 
-folds = a.nvim_create_augroup('Folds', {
-	clear = true
-})
-
-a.nvim_create_autocmd('FileType', {
-	group = folds,
-	pattern = 'c,rust',
-	callback = 'set foldmethod=indent'
-})
-
-a.nvim_create_autocmd('FileType', {
-	group = folds,
-	pattern = 'c,rust',
-	callback = 'set foldcolumn=1'
-})
 
