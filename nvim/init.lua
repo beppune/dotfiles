e1071718a8aba97fafcb57835f3ac2bab029627a
beppune <manzo.giuseppe@gmail.com>
2025-05-21 18:15:35 +0200

    split mappings for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,51 +6,10 @@ a = vim.api
 g = vim.g
 
 -- Settings
-o.background = "light"
-c.colorscheme('iceberg')
-c.language('en')
 o.number = true
-o.relativenumber = true
 o.tabstop = 4
-o.shiftwidth = 4
+o.shiftwidth = 0
+o.digraph = true
 
--- MAPPINGS
-
-local function map_key(mode, lhs, rhs, descj)
-	a.nvim_set_keymap(mode, lhs, rhs, { 
-		noremap = true,
-		desc = desc
-	})
-end
-
--- Insert Mode
-map_key('i', 'jj', '<Esc>', "Use 'jj' in insert mode to switch in normal mode")
-map_key('i', '<C-s>', '<Esc>:w<Esc>i', "In insert mode save changes and get back in insert mode")
-
--- Windows
-g.mapleader = " "
-map_key('n', '<Leader>v', '<C-w>v', "Split Window Vertically")
-map_key('n', '<Leader>r', '<C-w>r', "Rotate Windows")
-map_key('n', '<Leader>w', '<C-w>w', "Switch to next window")
-map_key('n', '<Leader>o', '<C-w>o', "Make this windows the only one visible")
-map_key('n', '<Leader>c', '<C-w>c', "Close current window")
-map_key('n', '<Leader>b', '<Esc>:30Lexplore!<Enter>', "Open netrw on the right")
-
--- Scroll in Normal Mode
--- g.mapleader = " "
-map_key('n', '<Leader>j', '<C-d>', "Scroll down half window")
-map_key('n', '<Leader>k', '<C-u>', "Scroll up hal window")
-map_key('n', '<Leader>h', '<C-e>', "Scroll down one line")
-map_key('n', '<Leader>l', '<C-y>', "Scroll up one line")
-
--- Tabs
--- g.mapleader = " "
-map_key('n', '<Leader>t', ':tabnew<Enter>', "New Tab")
-map_key('n', '<Leader>c', ':tabclose<Enter>', "Close Tab")
-map_key('n', '<Leader>n', ':tabnext<Enter>', "Next Tab")
-
--- Utils
-map_key('n', '<C-h>', ':nohlsearch<Enter>', "Clear search highlights")
-
-
+require('mappings')
 
